b1c6e8af1f7e9b5433bc489b68f750ebe1e20d37
phabos <phabos@users.noreply.github.com>
2018-12-13 22:02:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
 })
 
 let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
